79d670e81ae3ec00bfec03e5534ebd034be1d0f9
chabeer absal <chabeerabsal@gmail.com>
2022-05-01 21:59:44 +0530

    a

---
+++ sample.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"i feel demotivated i  was learning dsa for  past 4 months but cant able to solve a single problem that makes me frustrated";
+	return 0;
+}
